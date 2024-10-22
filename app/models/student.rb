ea2aabb18c649e3f5b067d75517dbc447260d42a
Joshua <jcerva21@msudenver.edu>
2024-10-22 12:52:29 -0600

    email_migrate

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :school_email, presence: true, uniqueness: true
+    #validates :school_email, presence: true, uniqueness: true
     validates :major, presence: true
     validates :graduation_date, presence: true
 
